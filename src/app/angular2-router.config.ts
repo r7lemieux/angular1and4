6146be06b0c6c107bd42b439cc81fec3f635ec21
r7lemieux <richard.lemieux@gmail.com>
2017-06-12 15:06:50 -0500

    srini2

--- src/app/angular2-router.config.ts
+++ src/app/angular2-router.config.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-// import { HeroesComponent } from './heroes/heroes.component';
-// import { HeroDetailComponent } from './heroes/hero-detail.component';
-import './dashboard/dashboardController';
-import './dashboard/dashboardComponent';
-import {DashboardComponent} from './dashboard/dashboardComponent';
-// import {HeroesComponent} from './heroes/heroes.component';
+// import { DashboardComponent } from './dashboard.component';
+// import { HeroesComponent } from './heroes.component';
+// import { HeroDetailComponent } from './hero-detail.component';
 
 const routes: Routes = [
-  { path: 'dahsboard2/component', component: DashboardComponent},
-  // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  // { path: 'dashboard', component: DashboardComponent },
   // { path: 'detail/:id', component: HeroDetailComponent },
   // { path: 'heroes', component: HeroesComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,7 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {UpgradeModule} from '@angular/upgrade/static';
 import {RouterModule} from '@angular/router';
-import {AppRoutingModule} from './app-routing.module';
-
+import {AppRoutingModule} from './angular2-router.config';
 
 @NgModule({
   declarations: [
